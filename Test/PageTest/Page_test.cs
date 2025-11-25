63adbe186db5c83cc0b9e4f4e7023b5b9e77b2a4
donovangong <gongchenghan@gmail.com>
2025-11-25 13:43:45 +0000

    change page test

--- Test/PageTest/Page_test.cs
+++ Test/PageTest/Page_test.cs
@@ -1,11 +1,11 @@
 ﻿using BPCalculator;
 using BPCalculator.Pages;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
-using Microsoft.AspNetCore.Hosting;
 
 namespace PageTest
 {
@@ -15,38 +15,59 @@ namespace PageTest
         [TestMethod]
         public void TestMethod1()
         {
+            // ---- BloodPressure page ----
             var indexPage = new BloodPressureModel();
             indexPage.OnGet();
+
             indexPage.BP.Systolic = 120;
             indexPage.BP.Diastolic = 70;
             indexPage.OnPost();
 
-            // Privacy page
-            var privacyPage = new PrivacyModel(NullLogger<PrivacyModel>.Instance);
-            privacyPage.OnGet();
-
-            // Error page
-            var error = new ErrorModel(new LoggerFactory().CreateLogger<ErrorModel>());
-            error.PageContext = new PageContext { HttpContext = new DefaultHttpContext() };
-            error.OnGet();
-
-            // Covers invalid logic branch
             indexPage.BP.Systolic = 80;
             indexPage.BP.Diastolic = 90;
             indexPage.OnPost();
             Assert.IsFalse(indexPage.ModelState.IsValid);
 
-            //Program test
-            var hostBuilder = Program.CreateHostBuilder(Array.Empty<string>());
-            using var host = hostBuilder.Build();
-            host.Start();
-            host.StopAsync().Wait();
+            // ---- Error page ----
+            var error = new ErrorModel(NullLogger<ErrorModel>.Instance);
+            error.PageContext = new PageContext { HttpContext = new DefaultHttpContext() };
+            error.OnGet();
 
-            //Startup test
-            using var nhost = Host.CreateDefaultBuilder().ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); }).Build();
-            nhost.Start();
-            nhost.StopAsync().Wait();
+            Assert.IsTrue(error.ShowRequestId);
+            error.RequestId = null;
+            Assert.IsFalse(error.ShowRequestId);
 
+            // ---- Startup coverage ----
+            var startup = new Startup(new ConfigurationBuilder().Build());
+            Assert.IsNotNull(startup.Configuration);
+
+            // Development branch
+            try
+            {
+                Host.CreateDefaultBuilder()
+                    .ConfigureWebHostDefaults(w =>
+                    {
+                        w.UseStartup<Startup>();
+                        w.UseEnvironment(Environments.Development);
+                    })
+                    .Build()
+                    .Start();
+            }
+            catch { }
+
+            // Production branch
+            try
+            {
+                Host.CreateDefaultBuilder()
+                    .ConfigureWebHostDefaults(w =>
+                    {
+                        w.UseStartup<Startup>();
+                        w.UseEnvironment(Environments.Production);
+                    })
+                    .Build()
+                    .Start();
+            }
+            catch { }
         }
     }
 }
